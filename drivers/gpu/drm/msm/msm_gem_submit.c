06093d283d8ffef7230db4feb2f516f17cfcaf54
stendro <33956801+stendro@users.noreply.github.com>
2018-01-22 09:04:25 +0100

    Linux 3.18.80
    From 3.18.79-80

--- drivers/gpu/drm/msm/msm_gem_submit.c
+++ drivers/gpu/drm/msm/msm_gem_submit.c
@@ -39,6 +39,10 @@ static struct msm_gem_submit *submit_create(struct drm_device *dev,
 	struct msm_gem_submit *submit;
 	uint64_t sz = sizeof(*submit) + (nr_bos * sizeof(submit->bos[0])) +
 		(nr_cmds * sizeof(submit->cmd[0]));
+		struct msm_gpu *gpu, uint32_t nr)
+{
+	struct msm_gem_submit *submit;
+	uint64_t sz = sizeof(*submit) + ((u64)nr * sizeof(submit->bos[0]));
 
 	if (sz > SIZE_MAX)
 		return NULL;
